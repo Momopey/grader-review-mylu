96b5dc295a665c79777cc7dca6ff75d224c39dcf
Momopey <m22461@gmail.com>
2023-03-02 12:21:30 -0800

    Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Grader script
+
+To run grader script 
+- Clone this repo
+- run `$bash run.sh <port-number>`
--- grade.sh
+++ grade.sh
@@ -10,12 +10,14 @@ cd student-submission
 cp ../TestListExamples.java ./
 cp -r ../lib/ ./lib
 
+#Check for files
 if [[ ! -f ListExamples.java ]] 
 then
     echo "Missing file ListExamples.java"
     exit 1
 fi
 
+#Try compoing
 javac -cp lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
 
 if [[ ! $? -eq 0 ]] 
@@ -24,11 +26,16 @@ then
     exit 1
 fi
 
+#Run tests, store out in results.txt
 java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > results.txt
+
 NUMTESTS=3
+#Grep for error messages
 TEST0=`grep "testMergeRightEnd(TestListExamples)" results.txt | wc -l`
 TEST1=`grep "testMergeLeftEnd(TestListExamples)" results.txt | wc -l`
 TEST2=`grep "testFilter(TestListExamples)" results.txt | wc -l`
+
+#Compute/show a grade
 echo $TEST0 $TEST1 $TEST2
 TOTALFAIL=`expr $TEST0 + $TEST1 + $TEST2`
 TOTALRIGHT=`expr $NUMTESTS - $TOTALFAIL`
